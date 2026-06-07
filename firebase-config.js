0a72649a65b6b425d02b7e301ea4cc38125453ce
AHAMED SHAHABAS K M <shahabas.it@protonmail.com>
2026-06-07 11:34:58 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCWPzWr-uwCW5I830FdCCjO-eJrvn0R8TI",
   authDomain: "chat-e97a4.firebaseapp.com",
+  databaseURL: "https://chat-e97a4-default-rtdb.firebaseio.com",
   projectId: "chat-e97a4",
   storageBucket: "chat-e97a4.firebasestorage.app",
   messagingSenderId: "434910789088",
   appId: "1:434910789088:web:c219d330ac5e91133c3f85"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
